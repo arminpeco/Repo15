85325c6984de150c6023eacd746bb33fedaefdaa
arminpeco <apeco1906@gmail.com>
2021-09-27 23:40:32 +0200

    Added file index.js

---
+++ ex04/src/index.js
@@ -0,0 +1,23 @@
+const milili=[10,25,4];
+
+function myArr(mili) {
+    'use strict';
+    // only change code below this line
+        mili[0]=4;
+        mili[1]=10;
+        mili[2]=25;
+        
+ 
+// only change code above this line
+return mili;
+
+}
+
+
+    
+
+
+console.log(myArr(milili)); // change this line
+
+
+module.exports=myArr;
