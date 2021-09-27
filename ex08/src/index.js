acfd4c07597b5d48b3a84ecbe35f5d803be7ab39
arminpeco <apeco1906@gmail.com>
2021-09-28 00:03:47 +0200

    Added file index.js

---
+++ ex08/src/index.js
@@ -0,0 +1,11 @@
+    // only change code below this line
+const sum=(num1,num2=1)=>
+{
+    return num1+num2;
+}
+
+// only change code above this line
+console.log(sum(4,6)) // change this line
+console.log(sum(3)) // change this line
+
+module.exports=sum;
